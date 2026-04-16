0978d8476551d14d9c0b67709ef9eabc1310b752
VikasChudmunge <vikaschudmunge5901@gmail.com>
2026-04-16 13:44:16 +0530

    freight rule child table create

---
+++ justsign_custom/justsign_custom/doctype/fright_role_fix_price/fright_role_fix_price.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, sanpra Softwares and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class FrightRoleFixPrice(Document):
+	pass
