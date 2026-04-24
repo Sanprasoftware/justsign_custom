84acb5481407ea4889fdd969f8faf25db5b62584
VikasChudmunge <vikaschudmunge5901@gmail.com>
2026-04-24 16:34:51 +0530

    PR REJEctd QTY

--- justsign_custom/overrides/serial_and_batch_bundle.py
+++ justsign_custom/overrides/serial_and_batch_bundle.py
@@ -10,21 +10,23 @@ from erpnext.stock.doctype.serial_and_batch_bundle.serial_and_batch_bundle impor
 class CustomSerialandBatchBundle(SerialandBatchBundle):
 	def validate_quantity(self, row, qty_field=None):
 		qty_field = self.get_qty_field(row, qty_field=qty_field)
-		qty = self.get_comparable_row_qty(row, qty_field)
-
 		precision = row.precision
-		if abs(abs(flt(self.total_qty, precision)) - abs(flt(qty, precision))) > 0.01:
+		total_qty = abs(flt(self.total_qty, precision))
+		comparable_quantities = self.get_comparable_row_quantities(row, qty_field)
+
+		if not any(abs(total_qty - abs(flt(qty, precision))) <= 0.01 for qty in comparable_quantities):
 			total_qty = frappe.format_value(abs(flt(self.total_qty)), "Float", row)
 			set_qty = frappe.format_value(abs(flt(row.get(qty_field))), "Float", row)
 			self.throw_error_message(
 				f"Total quantity {total_qty} in the Serial and Batch Bundle {bold(self.name)} does not match with the quantity {set_qty} for the Item {bold(self.item_code)} in the {self.voucher_type} # {self.voucher_no}"
 			)
 
-	def get_comparable_row_qty(self, row, qty_field):
+	def get_comparable_row_quantities(self, row, qty_field):
 		qty = row.get(qty_field)
+		quantities = [qty]
 
 		if qty_field == "qty" and row.get("stock_qty"):
-			return row.get("stock_qty")
+			quantities.append(row.get("stock_qty"))
 
 		if (
 			self.voucher_type in {"Purchase Receipt", "Purchase Invoice"}
@@ -34,6 +36,6 @@ class CustomSerialandBatchBundle(SerialandBatchBundle):
 			and row.get("uom") != row.get("stock_uom")
 			and flt(row.get("conversion_factor"))
 		):
-			return flt(qty) * flt(row.get("conversion_factor"))
+			quantities = [flt(qty) * flt(row.get("conversion_factor"))]
 
-		return qty
+		return [value for value in quantities if value is not None]
--- justsign_custom/public/js/custom_purchase_receipt.js
+++ justsign_custom/public/js/custom_purchase_receipt.js
@@ -1,7 +1,6 @@
 (() => {
-	if (!window.erpnext?.buying?.BuyingController) {
-		return;
-	}
+	const PATCH_FLAG = "justsign_custom_rejected_purchase_uom_patch";
+	const FORM_PATCH_FLAG = "justsign_custom_purchase_receipt_patch";
 
 	const get_entries_total_qty = (entries) => {
 		return (entries || []).reduce((total, row) => total + Math.abs(flt(row.qty) || 1.0), 0);
@@ -10,10 +9,6 @@
 	const should_use_purchase_uom_for_bundle = (item) => {
 		if (!item) {
 			return false;
-		}
-
-		if (item.is_rejected) {
-			return !!item.use_purchase_uom_for_rejected_bundle;
 		}
 
 		return !!item.use_purchase_uom_for_bundle;
@@ -63,11 +58,14 @@
 		}));
 	};
 
-	if (
-		erpnext.SerialBatchPackageSelector &&
-		!erpnext.SerialBatchPackageSelector.prototype
-			.justsign_custom_rejected_purchase_uom_patch
-	) {
+	const patch_selector = () => {
+		if (
+			!erpnext?.SerialBatchPackageSelector ||
+			erpnext.SerialBatchPackageSelector.prototype[PATCH_FLAG]
+		) {
+			return !!erpnext?.SerialBatchPackageSelector;
+		}
+
 		const original_update_bundle_entries =
 			erpnext.SerialBatchPackageSelector.prototype.update_bundle_entries;
 
@@ -91,97 +89,144 @@
 			}
 		};
 
-		erpnext.SerialBatchPackageSelector.prototype.justsign_custom_rejected_purchase_uom_patch =
-			true;
-	}
+		erpnext.SerialBatchPackageSelector.prototype[PATCH_FLAG] = true;
+		return true;
+	};
 
-	erpnext.buying.BuyingController.prototype.add_serial_batch_for_rejected_qty = function (
-		doc,
-		cdt,
-		cdn
-	) {
-		let item = locals[cdt][cdn];
-		let me = this;
-		let fields = ["has_batch_no", "has_serial_no"];
+	const patch_buying_controller = () => {
+		if (
+			!erpnext?.buying?.BuyingController ||
+			erpnext.buying.BuyingController.prototype[PATCH_FLAG]
+		) {
+			return !!erpnext?.buying?.BuyingController;
+		}
 
-		frappe.db.get_value("Item", item.item_code, fields).then((r) => {
-			if (!(r.message && (r.message.has_batch_no || r.message.has_serial_no))) {
-				return;
-			}
+		erpnext.buying.BuyingController.prototype.add_serial_batch_for_rejected_qty = function (
+			doc,
+			cdt,
+			cdn
+		) {
+			let item = locals[cdt][cdn];
+			let me = this;
+			let fields = ["has_batch_no", "has_serial_no"];
 
-			fields.forEach((field) => {
-				item[field] = r.message[field];
-			});
-
-			item.type_of_transaction = doc.is_return ? "Outward" : "Inward";
-			item.is_rejected = true;
-			item.use_purchase_uom_for_rejected_bundle = true;
-
-			new erpnext.SerialBatchPackageSelector(me.frm, item, (r) => {
-				if (!r) {
+			frappe.db.get_value("Item", item.item_code, fields).then((r) => {
+				if (!(r.message && (r.message.has_batch_no || r.message.has_serial_no))) {
 					return;
 				}
 
-				let qty = Math.abs(r.total_qty);
-				if (doc.is_return) {
-					qty = qty * -1;
-				}
+				fields.forEach((field) => {
+					item[field] = r.message[field];
+				});
 
-				let update_values = {
-					rejected_serial_and_batch_bundle: r.name,
-					use_serial_batch_fields: 0,
-					rejected_qty: qty,
+				item.type_of_transaction = doc.is_return ? "Outward" : "Inward";
+				item.is_rejected = true;
+				item.use_purchase_uom_for_rejected_bundle = false;
+
+				const selector_item = {
+					...item,
+					rejected_qty:
+						Math.abs(flt(item.rejected_qty || item.qty)) *
+						flt(item.conversion_factor || 1, precision("conversion_factor", item)),
 				};
 
-				if (r.warehouse) {
-					update_values.rejected_warehouse = r.warehouse;
-				}
+				new erpnext.SerialBatchPackageSelector(me.frm, selector_item, (bundle) => {
+					if (!bundle) {
+						return;
+					}
 
-				frappe.model.set_value(item.doctype, item.name, update_values);
+					let qty = Math.abs(bundle.total_qty);
+					if (doc.is_return) {
+						qty = qty * -1;
+					}
+
+					let update_values = {
+						rejected_serial_and_batch_bundle: bundle.name,
+						use_serial_batch_fields: 0,
+						rejected_qty:
+							qty / flt(item.conversion_factor || 1, precision("conversion_factor", item)),
+					};
+
+					if (bundle.warehouse) {
+						update_values.rejected_warehouse = bundle.warehouse;
+					}
+
+					frappe.model.set_value(item.doctype, item.name, update_values);
+				});
 			});
-		});
-	};
+		};
 
-	erpnext.buying.BuyingController.prototype.add_serial_batch_bundle = function (doc, cdt, cdn) {
-		let item = locals[cdt][cdn];
-		let me = this;
-		let fields = ["has_batch_no", "has_serial_no"];
+		erpnext.buying.BuyingController.prototype.add_serial_batch_bundle = function (doc, cdt, cdn) {
+			let item = locals[cdt][cdn];
+			let me = this;
+			let fields = ["has_batch_no", "has_serial_no"];
 
-		frappe.db.get_value("Item", item.item_code, fields).then((r) => {
-			if (!(r.message && (r.message.has_batch_no || r.message.has_serial_no))) {
-				return;
-			}
-
-			fields.forEach((field) => {
-				item[field] = r.message[field];
-			});
-
-			item.type_of_transaction = item.qty > 0 ? "Inward" : "Outward";
-			item.is_rejected = false;
-			item.use_purchase_uom_for_bundle = true;
-
-			new erpnext.SerialBatchPackageSelector(me.frm, item, (r) => {
-				if (!r) {
+			frappe.db.get_value("Item", item.item_code, fields).then((r) => {
+				if (!(r.message && (r.message.has_batch_no || r.message.has_serial_no))) {
 					return;
 				}
 
-				let qty = Math.abs(r.total_qty);
-				if (doc.is_return) {
-					qty = qty * -1;
-				}
+				fields.forEach((field) => {
+					item[field] = r.message[field];
+				});
 
-				let update_values = {
-					serial_and_batch_bundle: r.name,
-					use_serial_batch_fields: 0,
-					qty: qty / flt(item.conversion_factor || 1, precision("conversion_factor", item)),
-				};
+				item.type_of_transaction = item.qty > 0 ? "Inward" : "Outward";
+				item.is_rejected = false;
+				item.use_purchase_uom_for_bundle = true;
 
-				if (r.warehouse) {
-					update_values.warehouse = r.warehouse;
-				}
+				const selector = new erpnext.SerialBatchPackageSelector(me.frm, item, (bundle) => {
+					if (!bundle) {
+						return;
+					}
 
-				frappe.model.set_value(item.doctype, item.name, update_values);
+					let qty = Math.abs(bundle.total_qty);
+					if (doc.is_return) {
+						qty = qty * -1;
+					}
+
+					let update_values = {
+						serial_and_batch_bundle: bundle.name,
+						use_serial_batch_fields: 0,
+						qty: qty / flt(item.conversion_factor || 1, precision("conversion_factor", item)),
+					};
+
+					if (bundle.warehouse) {
+						update_values.warehouse = bundle.warehouse;
+					}
+
+					frappe.model.set_value(item.doctype, item.name, update_values);
+				});
+
+				selector.qty = Math.abs(flt(item.qty));
 			});
-		});
+		};
+
+		erpnext.buying.BuyingController.prototype[PATCH_FLAG] = true;
+		return true;
 	};
+
+	const install_patch = () => patch_selector() && patch_buying_controller();
+
+	const queue_patch_retry = () => {
+		if (install_patch()) {
+			return;
+		}
+
+		setTimeout(install_patch, 0);
+	};
+
+	queue_patch_retry();
+
+	frappe.ui.form.on("Purchase Receipt", {
+		setup() {
+			queue_patch_retry();
+		},
+		refresh(frm) {
+			if (!frm[FORM_PATCH_FLAG]) {
+				frm[FORM_PATCH_FLAG] = true;
+			}
+
+			queue_patch_retry();
+		},
+	});
 })();
